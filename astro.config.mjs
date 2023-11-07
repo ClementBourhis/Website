2a3c5dfc1b39713acf270f97c6469006ffc0a7da
Clément Bourhis <bourhis.clement29@gmail.com>
2023-11-07 15:44:45 +0100

    astro.config.mjs correction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://clementbourhis.github.io',
+  site: 'https://clementbourhis.github.io/Website',
   base: '/Website',
 })
