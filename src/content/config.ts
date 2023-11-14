d67cce04ddaa1c2028ceb4007c2f7161fff057d7
Clément Bourhis <bourhis.clement29@gmail.com>
2023-11-14 17:37:56 +0100

    video content module

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,8 @@ export const collections = {
 			tags: z.array(z.string()),
 			img: z.string(),
 			img_alt: z.string().optional(),
+			ressource: z.string().optional(),
+			ressource_type: z.string().optional(),
 		}),
 	}),
 };
