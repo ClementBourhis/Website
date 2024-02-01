bce717608257006070b3c4dead12ba62407f7db2
Clément Bourhis <bourhis.clement29@gmail.com>
2024-02-01 11:49:26 +0100

    Update Astro.js to V4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://clementbourhis.fr',
-})
+  prefetch: true,
+  experimental: {
+    clientPrerender: true,
+  },
+});
